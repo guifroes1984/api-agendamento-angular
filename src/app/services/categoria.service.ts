a58ce98886c45643378e82ba5cadf01f383dc59a
guifroes1984 <guilhermefroes26@gmail.com>
2026-02-23 17:59:08 -0300

    Criado o servico de categorias.

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Categoria } from '../models/categoria.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
-  constructor() { }
+  private apiUrl = `${environment.apiUrl}/categorias`;
+
+  constructor(private http: HttpClient) { }
+
+  public buscarCategoriasGanho(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(`${this.apiUrl}/ganhos}`);
+  }
+
+  public buscarCategoriasGasto(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(`${this.apiUrl}/gastos}`)
+  }
+
+  public buscarMetodosPagamentos(): Observable<Categoria[]> {
+    return this.http.get<Categoria[]>(`${this.apiUrl}/pagamentos`);
+  }
+
 }
